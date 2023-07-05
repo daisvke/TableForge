c58f6906554f4df6e25f1f78a7bd705efee2eaff
daisvke <daisuketanigawa@live.fr>
2023-07-05 16:53:03 +0200

    Table size is adaptable

--- tableForge.py
+++ tableForge.py
@@ -1,15 +1,15 @@
-
+padding = 8
 
 def generate_table(labels):
-	max_length = max(len(label) for label in labels) + 4
-	table_width = max_length + 4
+	max_length = max(len(label) for label in labels) + padding
+	table_width = max_length
 
-	centered_labels = [label.center(max_length + 2) for label in labels]
+	centered_labels = [label.center(table_width) for label in labels]
 
-	print('+' + '-' * (table_width - 2) + '+')
+	print('+' + '-' * table_width + '+')
 	for label in centered_labels:
-		print(f'|{label}|')
-		print('+' + '-' * (table_width - 2) + '+')
+		print(f"|{label}|")
+		print('+' + '-' * table_width + '+')
 
 user_input = input('Enter table labels (comma-separated): ')
 labels = [label.strip() for label in user_input.split(',')]
